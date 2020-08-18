926ce400ee4d8079c4e589b7ed06daf534b2a8ab
jake <jake-temp@Jacks-MacBook-Pro.local>
2020-08-18 16:41:31 +0100

    scaffold of routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>{/* <Link to="/about">About</Link> */}</li>
+          <li>
+            <Link to="/properties"> Property </Link>
+          </li>
+        </ul>
+
+        <Switch>
+          {/* <Route path="/about">
+            <About /> */}
+          {/* </Route> */}
+          <Route path="/properties">
+            <Properties />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Listings </h2>;
+}
+
+// function About() {
+//   return <h2>About</h2>;
+// }
+
+function Properties() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2> Details </h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Property />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a Property.</h3>
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-export default App;
+function Property() {
+  let { topicId } = useParams();
+  return <h3>Requested Property ID: {topicId}</h3>;
+}
